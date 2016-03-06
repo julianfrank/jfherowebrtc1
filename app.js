09a849f16bb254681b008be51663ac74f4edc031
Julian Frank <jfempire@yahoo.co.uk>
2016-03-06 18:32:15 +0530

    port from env

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(80);
+server.listen(process.env.PORT || 80);
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
