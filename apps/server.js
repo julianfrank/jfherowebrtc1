37a8069ffb8f070b58c81ba2f7ff17aa4694bc2d
Julian Frank <jfempire@yahoo.co.uk>
2016-03-15 16:15:33 +0530

    promisified

--- apps/server.js
+++ apps/server.js
@@ -16,11 +16,6 @@ function server() {
     const port = process.env.PORT || 80
     const log = helpers.log
 
-    //Mongoose Initialisaton
-    const mongoLabURL = process.env.mongoLabURL || require('../secrets.js').mongoDBConnectionString.toString()
-    mongoose.connect(mongoLabURL, () => log('Connected to ' + mongoLabURL))
-    let mongoConnection = mongoose.connection
-
     //Redis Initialisation
     const redisLabURL = process.env.redisLabURL || require('../secrets.js').redisConnectionString.toString()
     const redisLabPASS = process.env.redisLabPASS || require('../secrets.js').redisPassword.toString()
@@ -31,74 +26,147 @@ function server() {
         if (options.times_connected > 10) { return undefined; }// End reconnecting with built in error
         return Math.max(options.attempt * 100, 3000);// reconnect after
     }
-    let redisClient = redis.createClient({ url: redisLabURL, retry_strategy: redisRetryStrategy })
-    redisClient.auth(redisLabPASS)
+    let redisClient = null, redisSessionStore = null
+    //const initRedis = () => {
+    const initRedis = () => {
+        return new Promise((resolve, reject) => {
+            redisClient = redis.createClient({ url: redisLabURL, retry_strategy: redisRetryStrategy })
+            redisClient.auth(redisLabPASS, () => {
+                redisClient.info((err, reply) => {
+                    if (err) {
+                        log('Error Returned by Redis Server :' + err)
+                        reject()
+                    } else {
+                        log('Connected to ' + redisLabURL)
+                        redisSessionStore = new redisStore({ url: redisLabURL, client: redisClient, ttl: 360, prefix: 'session.' })// create new redis store for Session Management
+                        redisSessionStore.client.info((err, reply) => {
+                            if (err) {
+                                log('Error Returned by Redis Server :' + err)
+                                reject()
+                            } else {
+                                log('Redis Session Store Created Successfully')
+                                resolve()//Ensure we proceed only if Redis is connected and RedisSessionstore is working
+                            }
+                        })
+                    }
+                })
+            })
+        })
+    }
+    const quitRedis = (next) => {
+        return new Promise((resolve, reject) => {
+            redisClient.quit((err, res) => {
+                if (res === 'OK') {
+                    log('Closed Redis Connection: ' + redisLabURL)
+                    redisSessionStore.client.quit((err, res) => {
+                        if (res === 'OK') log('Alert! Redis Session Still seems Not Closed. Continuing to End Process Anyway')
+                        resolve()
+                    })
+                } else {
+                    log('Error: Redis Connection not Closed. Redis Server Says\tResult:' + res + '\tError:' + err + ' Continuing to End Process Anyway')
+                    reject()
+                }
+            })
+        })
+    }
 
+    //Mongoose Initialisaton
+    const mongoLabURL = process.env.mongoLabURL || require('../secrets.js').mongoDBConnectionString.toString()
+    let mongoConnection = null
+    const initMongoose = () => {
+        return new Promise((resolve, reject) => {
+            mongoose.connect(mongoLabURL, () => {
+                log('Connected to ' + mongoLabURL)
+                mongoConnection = mongoose.connection
+                //Start the server only if DB is Ready
+                mongoConnection.once('open', (err, db) => {
+                    if (err) {
+                        log('Problem Connecting with ' + mongoLabURL + 'due to ' + err + ' Going to exit')
+                        reject()
+                    } else {
+                        resolve()
+                    }
+                })
+            })
+        })
+    }
+    const closeMongoose = (next) => {
+        return new Promise((resolve, reject) => {
+            mongoConnection.close(() => {//Centralising Process exit so that DB/Session is closed before exit...
+                log('Closed Mongoose Connection : ' + mongoLabURL)
+                resolve()
+            })
+        })
+    }
 
     //Express Application Initialization
     const app = express()
-    app.engine('html', helpers.readHTML);// define the template engine [(filePath, options, callback)]
-    app.set('views', 'pages'); // specify the views directory
-    app.set('view engine', 'html'); // register the template engine
-    app.set('trust proxy', 1) // trust first proxy
-    app.use(session({
-        store: new redisStore({ url: redisLabURL, client: redisClient, ttl: 360, prefix: 'session.' }),// create new redis store.
-        secret: helpers.hourlyState(), saveUninitialized: false, resave: false
-    }));
-    //app.use(bodyParser.json());
-    //app.use(bodyParser.urlencoded({ extended: true }));
-    app.use('/static', express.static('static'));
+    const initExpress = () => {
+        return new Promise((resolve, reject) => {
+            app.engine('html', helpers.readHTML);// define the template engine [(filePath, options, callback)]
+            app.set('views', 'pages'); // specify the views directory
+            app.set('view engine', 'html'); // register the template engine
+            app.set('trust proxy', 1) // trust first proxy
+            app.use(session({
+                store: redisSessionStore,
+                secret: helpers.hourlyState(), saveUninitialized: false, resave: false
+            }));
+            //app.use(bodyParser.json());
+            //app.use(bodyParser.urlencoded({ extended: true }));
+            app.use('/static', express.static('static'));
 
-    //Express Routers
-    app.all('*', (req, res, next) => {
-        log('ips:' + req.ips + '\tprotocol:' + req.protocol + '\txhr:' + req.xhr + '\treq.session:' + !!(typeof req.session))
-        if (typeof req.session === 'undefined') {
-            console.trace()
-            exitProcess(0, 'Fatal Error: Session Service Failed. Possible Redis Failure. Going to exit Process.')
-        }
-        return next()
-    })
+            //Express Routers
+            app.all('*', (req, res, next) => {
+                log('ips:' + req.ips + '\tprotocol:' + req.protocol + '\txhr:' + req.xhr + '\treq.session:' + !!req.session)
+                if (typeof req.session === 'undefined') {
+                    console.trace()
+                    exitProcess(0, 'Fatal Error: Session Service Failed. Possible Redis Failure. Going to exit Process.')
+                }
+                return next()
+            })
 
-    app.all('/*.html', (req, res) => {// Need this to load test using loader.io
-        res.contentType('text/html')
-        res.render(req.params[0])
-    })
+            app.all('/*.html', (req, res) => {// Need this to load test using loader.io
+                res.contentType('text/html')
+                res.render(req.params[0])
+            })
 
-    app.all('/favicon.ico', (req, res) => {// Show my Pretty Face ;) on the favicon area
-        res.contentType('image/x-icon')
-        res.redirect('/static/favicon.ico')
-    })
+            app.all('/favicon.ico', (req, res) => {// Show my Pretty Face ;) on the favicon area
+                res.contentType('image/x-icon')
+                res.redirect('/static/favicon.ico')
+            })
 
-    app.all('/', (req, res) => {// Main page
-        if (!req.session.lastpath) {
-            req.session.lastpath = req.hostname + req.originalUrl + req.path
-            log('No lastpath in session. Setting ' + req.session.lastpath)
-        }
-        res.contentType('text/html')
-        res.render('jfmain')
-    })
+            app.all('/', (req, res) => {// Main page
+                if (!req.session.lastpath) {
+                    req.session.lastpath = req.hostname + req.originalUrl + req.path
+                    log('No lastpath in session. Setting ' + req.session.lastpath)
+                }
+                res.contentType('text/html')
+                res.render('jfmain')
+            })
 
-    //Start the server only if DB is Ready
-    mongoConnection.once('open', (err, db) => {
-        if (err) {
-            log('Problem Connecting with ' + mongoLabURL + ' Going to exit')
-            process.exit(1)
-        } else {
-            log('Going to start Server. Press Control+C to Exit')
-            app.listen(port, () => { log(helpers.readPackageJSON(__dirname, "name") + " " + helpers.readPackageJSON(__dirname, "version") + "\tStarted & Listening on port\t: " + port) })
-        }
-    })
+            resolve()
+        })
+    }
+
+
+
+    const startServer = () => {
+        log('Going to start Server. Press Control+C to Exit')
+        app.listen(port, () => { log(helpers.readPackageJSON(__dirname, "name") + " " + helpers.readPackageJSON(__dirname, "version") + "\tStarted & Listening on port\t: " + port) })
+    }
 
     // Start reading from stdin so we don't exit directly.
     process.stdin.resume();
-    process.on('SIGINT', () => { exitProcess(0, 'SIGINT') });
+    process.on('SIGINT', () => { stopProcess('SIGINT') });
 
-    function exitProcess(code, reason) {
+    const stopProcess = (reason) => {
         log('About to exit due to ' + reason);
-        mongoConnection.close(() => {//Centralising Process exit so that DB/Session is closed before exit...
-            log('Closed Mongoose Connection : ' + mongoLabURL)
-            process.exit(code)
-        })
+        closeMongoose().then(quitRedis).then(exitProcess).catch(exitProcess)
     }
+
+    const exitProcess = () => process.exit(0)
+
+    //Start the Initiation
+    initRedis().then(initMongoose).then(startServer).then(initExpress).catch(stopProcess)
 }
 module.exports.server = exports.server = server
