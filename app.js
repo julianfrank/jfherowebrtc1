7fb3edd4365f64e3210323583f0b90c6f2420a95
Julian Frank <jfempire@yahoo.co.uk>
2016-04-02 14:35:11 +0530

    do not loadd app if inside travis

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 'use strict'
 
 let app = require('./apps/server.js').server
-app()
+
+if (process.env.TRAVIS === 'YES') {
+    console.log('app.js\t:Not Proceeding to load app as this is inside Travis')
+} else {
+    console.log('app.js\t:Loading app')
+    app()
+}
