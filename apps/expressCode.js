30e7233088927a192d7ef054e870c28609f5eebf
Julian Frank <jfempire@yahoo.co.uk>
2016-04-17 14:36:40 +0530

    cors for css

--- apps/expressCode.js
+++ apps/expressCode.js
@@ -10,14 +10,15 @@ let initExpress = (processObjects) => {
         const bodyParser = require('body-parser') //Required to read the body
 
         let app = processObjects.app
-        let server = processObjects.server
-        
+
         app.locals.name = 'Julian Frank\'s WebRTC Application'
 
         app.engine('html', helpers.readHTML);// define the template engine [(filePath, options, callback)]
+        
         app.set('views', 'pages'); // specify the views directory
         app.set('view engine', 'html'); // register the template engine
         app.set('trust proxy', true) // trust first proxy
+        
         app.use(processObjects.expressSession({
             store: processObjects.redisSessionStore,
             secret: helpers.hourlyState(),
@@ -30,22 +31,23 @@ let initExpress = (processObjects) => {
         app.use(bodyParser.text({ type: 'text/html' }))
         app.use(bodyParser.urlencoded({ extended: true }));
         app.use(bodyParser.raw({ type: '*/*' }))
-        app.use('/static', processObjects.express.static('pages/static'));
-        app.use('/js', processObjects.express.static('pages/js'));
-        app.use('/css', processObjects.express.static('pages/css'));
+
         app.use((req, res, next) => {//Add CORS Support
             res.header("Access-Control-Allow-Origin", "*")//[TODO] Need to update to include only 'Trusted' domains
             res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
             next()
         })
+
+        app.use('/static', processObjects.express.static('pages/static'))
+        app.use('/js', processObjects.express.static('pages/js'))
+        app.use('/css', processObjects.express.static('pages/css'))
+
         app.all('*', (err, req, res, next) => { if (err) return reject('expressCode.js\t:Fatal Error: Error in Express Route ${err}. Going to exit Process.') })//Default Route to log All Access..Enters only if there is an error
         app.all('*', (req, res, next) => {//Default Route to log All Access
             log('expressCode.js\t:req.path:' + req.path + '\treq.isAuthenticated:' + req.isAuthenticated())
             if (typeof req.session === 'undefined') return reject('expressCode.js\t:Fatal Error: Session Service Failed. Possible Redis Failure. Going to exit Process.')
             return next()
         })
-
-        //server.on('request', app)
 
         process.nextTick(() => { return resolve(processObjects) })
     })
