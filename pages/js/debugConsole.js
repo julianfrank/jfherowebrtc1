4f798d3c6a28dfd71047673628dfd4a87bb1b56a
Julian Frank <jfempire@yahoo.co.uk>
2016-05-11 15:22:37 +0530

    DEBUG cONSOLE STABLE NOW

--- pages/js/debugConsole.js
+++ pages/js/debugConsole.js
@@ -1,8 +1,22 @@
 let debugText = "",
-debugUpdateScreen = true,
-debugBuffer = 10000,
-debugRefreshDelay=100
+    debugUpdateScreen = true,
+    debugBuffer = 4444,
+    debugRefreshDelay = 444
 
+window.onload = function () {
+    if (!document.getElementById('debugConsole')) {
+        let debugView = document.createElement("article")
+        debugView.appendChild(document.createTextNode("Debug View"))
+        debugView.className = 'supressed'
+        let debugPre = document.createElement("pre")
+        debugPre.id = 'debugConsole'
+        debugView.appendChild(debugPre)
+        let body = document.getElementsByTagName("body")[0].appendChild(debugView)
+        log('Julian Frank says:\t Your Debug Console is now Ready')
+    } else {
+        log('Julian Frank says:\t Your Debug Console was Ready')
+    }
+}
 
 function log(msg) {
     debugText = msg + '\n' + debugText
