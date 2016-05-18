b617bc91474822263101fd4514b68d4cfad43a93
Julian Frank <jfempire@yahoo.co.uk>
2016-05-18 16:00:03 +0530

    removed reliance on jQuery in debuglog

--- pages/js/debugConsole.js
+++ pages/js/debugConsole.js
@@ -1,18 +1,25 @@
-let log
+var log
 
 window.onload = function () {
 
     //Update this as per need
-    let debugText = '', debugUpdateScreen = true, debugBuffer = 4444, debugRefreshDelay = 44, DISPLAYDEBUGLOG = true
+    var debugText = '',//Default Text in hte debug Console...Replace with you app Intro/copyright etc
+        debugBuffer = 4444,//Number of Characters you want in to be visible Including scrolled text
+        debugRefreshDelay = 44,//How quickly you want the console to be refreshed. Use higher values if too much Logs are flowing and nothing difficult to read
+        DISPLAYDEBUGLOG = true,// use false to NOT show and use debuglog in your app
+        STREAMTOCONSOLE=false//Make this true if you want to log to console.log as well 
 
-    //Core Lod Update Function
+    //Do NOT Change this
+    var debugUpdateScreen = true
+
+    //Core Log Update Function
     log = function (msg) {
-        console.log(msg)
+        if(STREAMTOCONSOLE){console.log(msg)}//Kept to debug debugconsole app
         if (DISPLAYDEBUGLOG) {
-            debugText = msg + '\n' + debugText
-            debugText = debugText.slice(0, debugBuffer)
+            debugText = msg.concat('\n', debugText).slice(0, debugBuffer)
             if (debugUpdateScreen) {
-                $('#debugConsole').text(debugText)
+                document.getElementById('debugConsole').textContent = debugText
+                //$('#debugConsole').text(debugText)
                 debugUpdateScreen = false
                 setTimeout(function () {
                     debugUpdateScreen = true
@@ -24,29 +31,25 @@ window.onload = function () {
     //This inserts the log into the body
     if (DISPLAYDEBUGLOG) {
         if (!document.getElementById('debugConsole')) {
-            let debugView = document.createElement("article")
+            var debugView = document.createElement("aside")
             debugView.setAttribute('draggable', true)
 
             var debugViewStyle = document.createElement('style')
             debugView.className = 'debugConsole'
             debugViewStyle.innerHTML = ".debugConsole{width:25em;height:7em;position:absolute;bottom:1%;left:1%;z-index:111;max-height:44vh;color: white;opacity:0.16;background-color: black;overflow: visible;border-radius:7px;border-width:2px;border-color:black;border-style:solid;box-sizing:border-box}\n"
             debugViewStyle.innerHTML += ".debugConsole:hover{width:100%;height:100%;max-width:78%;overflow: auto;opacity:1;animation: mymove 1s infinite;animation-direction: alternate;}\n"
-            debugViewStyle.innerHTML += "@keyframes mymove {100% {box-shadow: 0px 0px 7px 7px darkgrey;}}"
+            debugViewStyle.innerHTML += "@keyframes mymove {100% {box-shadow: 0px 0px 7px 7px darkgrey;}}"//Comment this if need to use in low performance clients
             debugView.appendChild(debugViewStyle)
 
-            let debugPre = document.createElement("pre")
+            var debugPre = document.createElement("pre")
             debugPre.id = 'debugConsole'
             debugView.appendChild(debugPre)
 
-            let body = document.getElementsByTagName("body")[0].appendChild(debugView)
+            var body = document.getElementsByTagName("body")[0].appendChild(debugView)
 
             log('Julian Frank says: Your Debug Console is now Ready')
-
         } else {
-
             log('Julian Frank says: Your Debug Console was Ready')
-
         }
     }
 }
-
