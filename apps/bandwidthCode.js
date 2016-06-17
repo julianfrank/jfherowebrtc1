c6a4df3a478128f26fe59379174cad2a472ba418
Julian Frank <jfempire@yahoo.co.uk>
2016-06-17 14:01:14 +0530

    bandwidth library ready

--- apps/bandwidthCode.js
+++ apps/bandwidthCode.js
@@ -10,8 +10,10 @@ const bwToken = process.env.BWTOKEN || require('../secrets.js').bwToken
 const bwSecret = process.env.BWSECRET || require('../secrets.js').bwSecret
 
 //Initialise bandwidth with Authorisation codes
-let bandwidth = require('bandwidth')
+let bandwidth = require('node-bandwidth')
+console.log(bandwidth)
 let bwClient = new bandwidth.Client(bwUserID, bwToken, bwSecret)
+console.log(bwClient)
 
 let initBandwidth = (processObjects) => {
     log('info', 'Initializing bandwidth-node Service', logMeta)
@@ -24,41 +26,41 @@ let initBandwidth = (processObjects) => {
         processObjects.bandwidth = bandwidth
         processObjects.bwClient = bwClient
 
-/*        // bandwidth request authentication for Requests FROM bandwidth
-        app.get('/twauth', function (req, res) {
-            if (bandwidth.validateExpressRequest(req, twAuthToken)) {
-                let resp = new bandwidth.TwimlResponse()
-                resp.say('Julian Frank Says hello bandwidth!')
-
-                res.type('text/xml')
-                res.send(resp.toString())
-            }
-            else {
-                res.status(403).send('you are not bandwidth. Buzz off.')
-            }
-        })
-        // Insert the bandwidth App here
-        app.get('/bandwidthApp', (req, res) => {
-            res.contentType('text/html')
-            userMan.getLoggedUsers().then((userList) => {
-                let thisUser = (req.session.passport) ? (req.session.passport.user) : ('Guest')
-                processObjects.genTwCap(thisUser, ['in', 'out']).then((token) => {
-                    let userInfo = {
-                        user: thisUser,
-                        appVer: helpers.readPackageJSON(__dirname, "version"),
-                        loggedUserList: userList,
-                        twToken: token
+        /*        // bandwidth request authentication for Requests FROM bandwidth
+                app.get('/twauth', function (req, res) {
+                    if (bandwidth.validateExpressRequest(req, twAuthToken)) {
+                        let resp = new bandwidth.TwimlResponse()
+                        resp.say('Julian Frank Says hello bandwidth!')
+        
+                        res.type('text/xml')
+                        res.send(resp.toString())
                     }
-                    if (req.isAuthenticated()) {
-                        res.render('bandwidthApp.html', userInfo)
-                    } else {
-                        res.render('jfmain.html', userInfo)
+                    else {
+                        res.status(403).send('you are not bandwidth. Buzz off.')
                     }
                 })
-
-            })
-        })
-*/
+                // Insert the bandwidth App here
+                app.get('/bandwidthApp', (req, res) => {
+                    res.contentType('text/html')
+                    userMan.getLoggedUsers().then((userList) => {
+                        let thisUser = (req.session.passport) ? (req.session.passport.user) : ('Guest')
+                        processObjects.genTwCap(thisUser, ['in', 'out']).then((token) => {
+                            let userInfo = {
+                                user: thisUser,
+                                appVer: helpers.readPackageJSON(__dirname, "version"),
+                                loggedUserList: userList,
+                                twToken: token
+                            }
+                            if (req.isAuthenticated()) {
+                                res.render('bandwidthApp.html', userInfo)
+                            } else {
+                                res.render('jfmain.html', userInfo)
+                            }
+                        })
+        
+                    })
+                })
+        */
         resolve(processObjects)
     })
 }
